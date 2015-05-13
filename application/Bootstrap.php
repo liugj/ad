f55802aac413c88a99cb287269937c96f48ba594
work <liugj@126.com>
2015-05-13 17:36:35 +0800

    first commit

---
+++ application/Bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+   /* bootstrap class should be defined under ./application/Bootstrap.php */
+   class Bootstrap extends Yaf\Bootstrap_Abstract {
+        public function _initConfig(Yaf\Dispatcher $dispatcher) {
+        }
+        public function _initPlugin(Yaf\Dispatcher $dispatcher) {
+        }
+   }
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+define("APP_PATH",  realpath(dirname(__FILE__))); 
+$app  = new Yaf\Application(APP_PATH . "/config/application.ini");
+$app->bootstrap() ->run();
