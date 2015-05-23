187b83d613d90be237ec9b924c11130d3e95973f
work <liugj@126.com>
2015-05-23 22:38:39 +0800

    delete modules/controllers/index:

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -6,6 +6,7 @@ class Bootstrap extends Yaf\Bootstrap_Abstract {
 	}
 	public function _initPlugin(Yaf\Dispatcher $dispatcher) {
 	}
+
 	//public function _initRoute(Yaf\Dispatcher $dispatcher) {
 	//}
 }
