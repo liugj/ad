4aa2b39c3dae5e80cfbdec59fa3579ebe6ab8feb
work <liugj@126.com>
2015-05-23 22:35:17 +0800

    iieeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -1,8 +1,11 @@
 <?php
    /* bootstrap class should be defined under ./application/Bootstrap.php */
-   class Bootstrap extends Yaf\Bootstrap_Abstract {
-        public function _initConfig(Yaf\Dispatcher $dispatcher) {
-        }
-        public function _initPlugin(Yaf\Dispatcher $dispatcher) {
-        }
-   }
+class Bootstrap extends Yaf\Bootstrap_Abstract {
+	public function _initConfig(Yaf\Dispatcher $dispatcher) {
+                Yaf\Registry::set('config', $dispatcher->getApplication()-> getConfig());
+	}
+	public function _initPlugin(Yaf\Dispatcher $dispatcher) {
+	}
+	//public function _initRoute(Yaf\Dispatcher $dispatcher) {
+	//}
+}
