6069ff474d3a07a1c3f1491a421dc0e581f28017
work <liugj@126.com>
2015-07-24 18:50:15 +0800

    修改按照目录获取

--- application/modules/Newad/controllers/Exchange.php
+++ application/modules/Newad/controllers/Exchange.php
@@ -7,37 +7,21 @@ class ExchangeController extends Yaf\Controller_Abstract {
 		$adConfig   =  new Yaf\Config\Ini(APP_PATH. '/config/ad.ini');
 		$size       = $adConfig->size->toArray();
 		$seq        = $adConfig->seq->toArray();
-		$mediaUnionRaw = $adConfig->mediaUnion->toArray();
-               // 根据siteId 和auId 找媒体联盟
-                $mediaUnion = array();
-		foreach ($mediaUnionRaw as $key=>$value){
-			$mediaUnion[$key]  = $value;
-			$mediaList         =  explode(',', $value);
-			for ($i=0; $i<sizeof($mediaList)-1; $i++) {
-				$mediaUnion[$mediaList[$i]]  = str_replace($mediaList[$i], $key, $value);
-			}
-		}
-                 //选中媒体顺序
-                $currentMediaUnion = array();
-                $requestMedia = sprintf('%d_%d', $siteId, $auId);
-		if (isset($mediaUnion[$requestMedia])) {
-			$currentMediaUnion['sites'][$auId] = $siteId; 
-			$tempList    = explode(',', $mediaUnion[$requestMedia]);
-			for($i=0; $i<sizeof($tempList)-1; $i++){
-				$temp       = explode('_', $tempList[$i]);
-				$currentMediaUnion['sites'][$temp[1]] = $temp[0];
-			}
-			$currentMediaUnion['size'] = explode('_', $size[$tempList[$i]]);
-                        $currentMediaUnion['seq']  = $seq;
-		}
-
+        $filename   = sprintf('%s/config/media/%d/%d', APP_PATH, $auId, $siteId);
+        $content    = trim(file_get_contents($filename));
+        $currentMediaUnion = array('seq'=>$seq);
+        $temp = explode(',', $content);
+        $currentMediaUnion['size'] = explode('_', $size[$temp[1]]);
+        $currentMediaUnion['sites'] [$auId] = $siteId;
+        list($nextSiteId, $nextAuId) = explode('_', $temp[0]);
+        $currentMediaUnion['sites'] [$auId] = $siteId;
+        $currentMediaUnion['sites'] [$nextAuId] = $nextSiteId;
 		$this->getView()->assign("media",  $currentMediaUnion);
 		$this->getView()->assign("auId",   $auId);
-               //header('Content-Type: text/plain');
-                                  if ($auId==0 || $auId==3) {
-                        header('Content-Type: text/plain');
-                }else{
-                        header('Content-Type: text/html');
-                }
+		if ($auId==0|| $auId==3) {
+			header('Content-Type: text/plain');
+		}else{
+			header('Content-Type: text/html');
+		}
 	}
 }
