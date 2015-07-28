3942bdfd84ee123cf28ca087c6661bc290c65013
work <liugj@126.com>
2015-07-28 22:17:39 +0800

    增加新媒体

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -1,29 +1,23 @@
 <?php
    /* bootstrap class should be defined under ./application/Bootstrap.php */
 class Bootstrap extends Yaf\Bootstrap_Abstract {
-	public function _initConfig(Yaf\Dispatcher $dispatcher) {
-                Yaf\Registry::set('config', $dispatcher->getApplication()-> getConfig());
-	}
-	public function _initPlugin(Yaf\Dispatcher $dispatcher) {
-	}
+    public function _initConfig(Yaf\Dispatcher $dispatcher) {
+        Yaf\Registry::set('config', $dispatcher->getApplication()-> getConfig());
+    }
+    public function _initPlugin(Yaf\Dispatcher $dispatcher) {
+    }
 
-	public function _initRoute(Yaf\Dispatcher $dispatcher) {
-		//$route = new Yaf\Route_Map(
-		//		　　'ads/([a-zA-Z]+)',
-		//		　　array(
-		//			　　　　'controller' => 'exchange',
-                //                                'module' => 'newad',
-		//			　　　　'action' => 's'
-		//			　　),
-		//		);
-		//$router->addRoute('ads', $route);
-#                $route = new Yaf_Route_Regex(
-#   　　'#product/([a-zA-Z-_0-9]+)#',
-#   　　array(
-#　　　　　　'controller' => 'products',
-#　　　　　　'action' => 'view'
-#   　　)
-#   );
-#   $router->addRoute('product', $route);
-	}
+    public function _initRoute(Yaf\Dispatcher $dispatcher) {
+        $dispatcher->getRouter()->addRoute("ads",
+                new Yaf\Route\Rewrite(
+                    "/ads/adexchange/ad.html", //match request uri leading "/product"
+                    array(
+                        'controller' => "exchange",  //route to product controller,
+                        'action' =>'s',
+                        'module' =>'newad'
+                        )
+                    )
+                );
+
+    }
 }
