e8129ced5aca5839c64281a79f7c61895f8d682d
work <liugj@126.com>
2015-07-28 22:53:18 +0800

    迁移代码

--- application/modules/Newad/controllers/Exchange.php
+++ application/modules/Newad/controllers/Exchange.php
@@ -1,27 +1,33 @@
 <?php 
 class ExchangeController extends Yaf\Controller_Abstract {
-	public function  SAction() {//默认Action
-		$siteId = intval($this->getRequest()->get('siteId', 0));
-		$auId   = intval($this->getRequest()->get('auId', 0));
+    public function  SAction() {//默认Action
+        $siteId = intval($this->getRequest()->get('siteId', 0));
+        $auId   = intval($this->getRequest()->get('auId', 0));
+        $ver    = $this->getRequest()->get('ver', '');     
+        $id    = $this->getRequest()->get('id', 0);     
 
-		$adConfig   =  new Yaf\Config\Ini(APP_PATH. '/config/ad.ini');
-		$size       = $adConfig->size->toArray();
-		$seq        = $adConfig->seq->toArray();
+        $adConfig   =  new Yaf\Config\Ini(APP_PATH. '/config/ad.ini');
+        $size       = $adConfig->size->toArray();
+        $seq        = $adConfig->seq->toArray();
+        $currentMediaUnion = array('seq'=>$seq,'sites'=>array());
         $filename   = sprintf('%s/config/media/%d/%d', APP_PATH, $auId, $siteId);
-        $content    = trim(file_get_contents($filename));
-        $currentMediaUnion = array('seq'=>$seq);
-        $temp = explode(',', $content);
-        $currentMediaUnion['size'] = explode('_', $size[$temp[1]]);
-        $currentMediaUnion['sites'] [$auId] = $siteId;
-        list($nextSiteId, $nextAuId) = explode('_', $temp[0]);
-        $currentMediaUnion['sites'] [$auId] = $siteId;
-        $currentMediaUnion['sites'] [$nextAuId] = $nextSiteId;
-		$this->getView()->assign("media",  $currentMediaUnion);
-		$this->getView()->assign("auId",   $auId);
-		if ($auId==0|| $auId==3) {
-			header('Content-Type: text/plain');
-		}else{
-			header('Content-Type: text/html');
-		}
-	}
+        if (file_exists($filename)) {
+            $content    = trim(file_get_contents($filename));
+            $temp = explode(',', $content);
+            $currentMediaUnion['size'] = explode('_', $size[$temp[1]]);
+            $currentMediaUnion['sites'] [$auId] = $siteId;
+            list($nextSiteId, $nextAuId) = explode('_', $temp[0]);
+            $currentMediaUnion['sites'] [$auId] = $siteId;
+            $currentMediaUnion['sites'] [$nextAuId] = $nextSiteId;
+        }
+        $this->getView()->assign("media",  $currentMediaUnion);
+        $this->getView()->assign("auId",   $auId);
+        $this->getView()->assign("ver",   $ver);
+        $this->getView()->assign("id",   $id);
+        if ($auId==0|| $auId==3) {
+            header('Content-Type: text/plain');
+        }else{
+            header('Content-Type: text/html');
+        }
+    }
 }
