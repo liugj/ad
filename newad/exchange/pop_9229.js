6d0596b3402464094fec6048a4da1de0395d7212
work <liugj@126.com>
2015-08-07 20:31:46 +0800

    buttons

--- newad/exchange/pop_9229.js
+++ newad/exchange/pop_9229.js
@@ -8,7 +8,7 @@ document.write('<script type="text/javascript">function closeDiv(innerCall){if(d
 document.write('</script>');
 document.write('<div style="z-index: 1000; right: 0px; bottom: 0px; overflow-x: hidden; overflow-y: hidden; position: fixed; width: 300px; height: 250px; " id="PcPoPmarket_9229">');
 document.write('<div style="z-index:1000; POSITION: absolute; right:0; height:30px; overflow:hidden;">');
-document.write('<span style="width:18px; height:18px; cursor:pointer; float:right; margin-left:1px; margin-top:12px; display:inline; background:url(/assets/img/buttons.png) no-repeat; hover{background-position:-39px -20px};margin-right:10px; width:15px; background-position:-39px 0;" onclick="closeDiv()"></span>');
+document.write('<span style="width:18px; height:18px; cursor:pointer; float:right; margin-left:1px; margin-top:12px; display:inline; background:url(http://www.adhufeng.com/assets/img/buttons.png) no-repeat; hover{background-position:-39px -20px};margin-right:10px; width:15px; background-position:-39px 0;" onclick="closeDiv()"></span>');
 document.write('</span></div>');
 document.write('<iframe id="popFrame" scrolling="no" frameborder="0" width="300" allowtransparency="true" height="250" src="http://www.adhufeng.com/newad/exchange/ad_9229_0.html" target="_blank">-</iframe>');
 document.write('</div>');
